9fb642350f14b4f9a99691698907d3a9b1897deb
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2022-03-25 15:27:00 -0400

    bcs

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,35 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';   //ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
-
-const httpLink = createHttpLink({       //h a new link to the GraphQL server at its /graphql endpoint with createHttpLink()
-  uri: 'http://localhost:3001/graphql',
+const httpLink = createHttpLink({
+  uri: '/graphql',
 });
-const client = new ApolloClient({      // the ApolloClient() constructor to instantiate the Apollo Client instance and create the connection to the API endpoint. We also instantiate a new cache object using new InMemoryCache().
+
+const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-})
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </ApolloProvider>
   );
 }
