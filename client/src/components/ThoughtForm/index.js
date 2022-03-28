a2ee783416fe5649face44a17aa8ffe35807cfff
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2022-03-28 03:51:08 -0400

    implemented and added reaction form and mutation

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,63 +1,72 @@
-import React, {useState} from 'react';
-import {useMutation} from "@apollo/client";
+import React, { useState } from 'react';
+import { useMutation } from "@apollo/client";
 import { ADD_THOUGHT } from '../../utils/mutations';
 import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
-const [thoughtText, setText] = useState('');
-const [characterCount, setCharacterCount] = useState(0);
+    const [thoughtText, setText] = useState('');
+    const [characterCount, setCharacterCount] = useState(0);
 
-const [addThought, {error}] = useMutation(ADD_THOUGHT, {
-    update(cache, {data: {addThought}}) {
-        //read what's currently in the cache
-        const {thoughts} = cache.readQuery({query: QUERY_THOUGHTS});
+    const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+        update(cache, { data: { addThought } }) {
+            try {
+                // could potentially not exist yet, so wrap in a try...catch
+                const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+                cache.writeQuery({
+                    query: QUERY_THOUGHTS,
+                    data: { thoughts: [addThought, ...thoughts] }
+                });
+            } catch (e) {
+                console.error(e);
+            }
 
-        //prepend the newest thought to the from of the array
-        cache.writeQuery({
-            query: QUERY_THOUGHTS,
-            data: {thoughts: [addThought, ...thoughts]}
-        });
-    }
-});
+            // update me object's cache, appending new thought to the end of the array
+            const { me } = cache.readQuery({ query: QUERY_ME });
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+            });
+        }
+    });
 
-const handleChange = event => {
-    if (event.target.value.length <= 280) {
-        setText(event.target.value);
-        setCharacterCount(event.target.value.length);
-    }
-};
+    const handleChange = event => {
+        if (event.target.value.length <= 280) {
+            setText(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    };
 
-const handleFormSubmit = async event => {
-    event.preventDefault();
+    const handleFormSubmit = async event => {
+        event.preventDefault();
 
-    try {
-        //add thought to database
-        await addThought({
-            variables: {thoughtText}
-        });
+        try {
+            //add thought to database
+            await addThought({
+                variables: { thoughtText }
+            });
 
-        //clear form value
-        setText('');
-        setCharacterCount(0);
-    } catch (e) {
-        console.error(e);
-    }
-};
+            //clear form value
+            setText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
 
 
     return (
-        <div> 
+        <div>
             <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
                 {error && <span className="ml-2">Something went wrong...</span>}
             </p>
             <form className="flex-row justify-center justify-space-between-md align-stretch on" onSubmit={handleFormSubmit}>
                 <textarea
-                placeholder="Here's a new thought..."
-                value={thoughtText}
-                className="form-input col-12 col-md-9"
-                onChange={handleChange}
+                    placeholder="Here's a new thought..."
+                    value={thoughtText}
+                    className="form-input col-12 col-md-9"
+                    onChange={handleChange}
                 ></textarea>
                 <button className="btn col-12 col=md-3" type="submit">
                     Submit
